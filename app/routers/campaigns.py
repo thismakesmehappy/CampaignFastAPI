059354d97ffdbf467dac5e142b5f8845a1cdd40a
Bernardo Margulis <bernardo@thismakesmehappy.co>
2026-04-29 13:06:20 -0400

    Replace campaign routers to use services

--- app/routers/campaigns.py
+++ app/routers/campaigns.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud
 from app.database import get_db
-from app.exceptions import NotFoundError
 from app.schema import (
     CampaignRead,
     CampaignCreate,
@@ -13,44 +11,28 @@ from app.schema import (
 )
 from app.schema.error import ErrorResponse
 
+from app.services import campaign as campaign_service
+
 router = APIRouter(prefix="/campaigns", tags=["campaigns"])
 
 _404 = {404: {"model": ErrorResponse}}
 
 @router.post("/", response_model=CampaignRead, status_code=201)
 async def create_campaign(data: CampaignCreate, db: AsyncSession = Depends(get_db)):
-    return await crud.create_campaign(db, data)
+    return await campaign_service.create(db, data)
 
 @router.get("/", response_model=PaginatedResponse[CampaignRead], status_code=200)
 async def list_campaigns(pagination: PaginatedFilter = Depends(), db: AsyncSession = Depends(get_db)):
-    campaigns_list = await crud.list_campaigns(db, pagination)
-    total = await crud.get_total_number_of_campaigns(db)
-    has_more = pagination.offset + len(campaigns_list) < total
-    response = PaginatedResponse(items=campaigns_list, has_more=has_more, total=total, offset=pagination.offset, limit=pagination.limit)
-    return response
+    return await campaign_service.list_campaigns(db, pagination)
 
 @router.get("/{campaign_id}", response_model=CampaignRead, status_code=200, responses=_404)
 async def get_campaign(campaign_id: int, db: AsyncSession = Depends(get_db)):
-    campaign = await crud.get_campaign(db, campaign_id)
-    errors = NotFoundError()
-    if not campaign:
-        errors.capture("Campaign")
-        errors.raise_if_any()
-    return campaign
+    return await campaign_service.get(db, campaign_id)
 
 @router.patch("/{campaign_id}", response_model=CampaignRead, status_code=200, responses=_404)
 async def update_campaign(campaign_id: int, data: CampaignUpdate, db: AsyncSession = Depends(get_db)):
-    campaign = await crud.update_campaign(db, campaign_id, data)
-    errors = NotFoundError()
-    if not campaign:
-        errors.capture("Campaign")
-        errors.raise_if_any()
-    return campaign
+    return await campaign_service.update(db, campaign_id, data)
 
 @router.delete("/{campaign_id}", status_code=204, responses=_404)
 async def delete_campaign(campaign_id: int, db: AsyncSession = Depends(get_db)):
-    deleted = await crud.delete_campaign(db, campaign_id)
-    errors = NotFoundError()
-    if not deleted:
-        errors.capture("Campaign")
-        errors.raise_if_any()
+    await campaign_service.delete(db, campaign_id)
